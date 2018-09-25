7c2751edd300934ea641fab9cc55c42d386db6d1
Christos Loukopoulos <s5063638@bournemouth.ac.uk>
2018-09-25 17:32:42 +0100

    glew exceptions

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 
 #include <exception>
+#include <GL/glew.h>
 
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
@@ -14,7 +15,17 @@ int main(int argc, char *argv[])
 
   SDL_Window *window = SDL_CreateWindow("Triangle",
     SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-    WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);
+    WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+
+  if (!SDL_GL_CreateContext(window))
+  {
+	  throw std::exception();
+  }
+
+  if (glewInit() != GLEW_OK)
+  {
+	  throw std::exception();
+  }
 
   bool quit = false;
 
